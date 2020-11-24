0d034040d3ec19b0f641392a0ca550895ea521c7
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2020-11-24 23:00:52 +1300

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 const SapphireServer = require('../sapphire-server.js');
 const server = new SapphireServer({
   core: {
-    jar: 'spigot-1.16.3.jar',
-    args: ['-Xmx4G', '-Xms2G'],
+    jar: 'spigot-1.16.3.jar', //Server Jar Name
+    args: ['-Xmx4G', '-Xms2G'], //Server Ram
   },
   remote: {
-    port: 35561
+    port: 35565 //Websocket Port
   }
 });
 
