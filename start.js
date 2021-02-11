f84d5c7d94b21ee8d245b865b7026644db387349
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-02-12 09:55:27 +1300

    add backup statement

--- start.js
+++ start.js
@@ -3,7 +3,9 @@ const server = new SapphireServer({
   core: {
     jar: 'server.jar', //Server Jar Name
     args: ['-Xmx4G', '-Xms2G'], //Server Ram
-    authorization: "auth code here"
+    authorization: "auth code here", //HTTP Auth COde
+    restartTime: 10, //time in seconds that the server will wait to restart
+    backups: false //enable the backup feature (backs up the server every week)
   },
   remote: {
     port: 35565 //Websocket Port
