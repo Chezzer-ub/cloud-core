0c39ed57b899fb2166ce5513b1074a73b94acace
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-02-12 19:45:05 +1300

    add port option

--- start.js
+++ start.js
@@ -1,14 +1,15 @@
-const SapphireServer = require('./sapphire-server.js');
+const SapphireServer = require('../sapphire-server.js');
 const server = new SapphireServer({
   core: {
-    jar: 'server.jar', //Server Jar Name
-    args: ['-Xmx4G', '-Xms2G'], //Server Ram
-    authorization: "auth code here", //HTTP Auth COde
+    jar: 'paper-1.16.4-310.jar', //Server Jar Name
+    args: ['-Xmx12G', '-Xms12G'], //Server Ram
+    port: 25533, //Minecraft Server Port
+    authorization: "b2x3AqYkjT2T6F5G", //HTTP Auth COde
     restartTime: 10, //time in seconds that the server will wait to restart
-    backups: false //enable the backup feature (backs up the server every week)
+    backups: true //enable the backup feature (backs up the server every week)
   },
   remote: {
-    port: 35565 //Websocket Port
+    port: 35533 //Websocket Port
   }
 });
 
