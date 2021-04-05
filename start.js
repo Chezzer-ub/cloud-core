890d098ecaee1a4798d9458810a0f6a610c02c83
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-04-05 12:24:37 +1200

    Update start.js

--- start.js
+++ start.js
@@ -5,7 +5,8 @@ const server = new SapphireServer({
     args: ['-Xmx12G', '-Xms12G'], //Server Ram
     port: 25533, //Minecraft Server Port
     authorization: "b2x3AqYkjT2T6F5G", //HTTP Auth COde
-    backups: true //enable the backup feature (backs up the server every week (Alternatively you can do "server.backup()" after "server.start()" at the bottom to back it up immediately OR do /backup in the console))
+    backups: true, //enable the backup feature (backs up the server every week (Alternatively you can do "server.backup()" after "server.start()" at the bottom to back it up immediately OR do /backup in the console))
+    restartTime: 10 //Time in seconds that the server will wait before restarting after a stop or /restart
   },
   remote: {
     port: 35533 //Websocket Port
