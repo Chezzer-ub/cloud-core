c4138e419a382dd0e05981ede2ae51368d11fc06
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-06-16 19:09:06 +1200

    Update to VERSION 2?!?!?!??!!?

--- cloud-core.js
+++ cloud-core.js
@@ -1,310 +1,328 @@
-const EventsEmitter = require('events');
+const Events = require('events');
 const { spawn, exec } = require('child_process');
-const WebSocketServer = require('websocket').server;
-const http = require('http');
+const express = require('express');
+const ws = require('ws');
 const fs = require('fs');
-const axios = require('axios');
-var messageHandler = new EventsEmitter();
+const usage = require('usage');
 
-const defaultConfig = {
-  core: {
-    jar: 'server.jar',
-    args: ['-Xmx2G', '-Xms1G'],
-    authorization: "",
-    backups: false,
-    serverPort: 25565,
-    restartTime: 10
-  },
-  remote: {
-    port: 35565
-  }
-};
-
-class CloudCore extends EventsEmitter {
+class CloudCore extends Events {
   /**
    * Create server instance.
    *
    * @param {array} config Array of options, help on git.
    */
   constructor(config) {
+    //load in Events
     super();
-    this.config = config || defaultConfig;
-    this.messageHandler = messageHandler
-    this.modules = [];
 
-    process.on('exit', () => this.stop());
-    process.on('close', () => this.stop());
+    //construct config with default values
+    const defaultConfig = {
+      core: {
+        jar: config.core.jar || 'server.jar',
+        args: config.core.args || ['-Xmx2G', '-Xms1G'],
+        authorization: config.core.authorization || "hackme",
+        backups: {
+          enabled: config.core.backups.enabled || false,
+          directory: config.core.backups.directory || "./backup/",
+          time: config.core.backups.time || "weekly" //can be "weekly", "monthly"
+        },
+        port: config.core.port || 25565
+      },
+      remote: {
+        bind: config.remote.bind || "0.0.0.0",
+        port: config.remote.port || 35565
+      }
+    };
 
-    if (this.config.core.backups) {
+    config = defaultConfig;
+
+    this.config = config; //config from construct
+    this.wsServer = null; //websocket
+    this.httpServer = null; //express
+    this.minecraftServer = null; //java spawn command
+
+    process.on('exit', () => {
+      this.stop()
+      if (this.minecraftServer) this.minecraftServer.kill();
+    });
+    process.on('close', () => {
+      this.stop()
+      if (this.minecraftServer) this.minecraftServer.kill();
+    });
+
+    this.wsServer = new ws.Server({ noServer: true });
+    this.wsServer.on('connection', socket => {
+      //receive command from client
+      socket.on('message', (message) => {
+        try {
+          message = JSON.parse(message);
+        } catch (e) {
+          socket.send(e);
+        }
+
+        let usertext = "";
+        if (message.user) {
+          usertext = message.user+" is ";
+        }
+
+        //parse actions
+        if (message.action == "command") {
+          this.send(message.command, message.user || undefined);
+        } else if (message.action == "stop") {
+          this.log(`[Cloud Core] ${usertext}Stopping the server...`);
+          this.stop(() => {
+            this.log(`[Cloud Core] Successfully stopped the server`);
+          });
+        } else if (message.action == "kill") {
+          this.log(`[Cloud Core] ${usertext}Attempting to stop the server...`);
+          this.stop(() => {
+            this.log(`[Cloud Core] Killing the server...`);
+            process.exit();
+          })
+          setTimeout(() => {
+            this.minecraftServer.kill();
+            process.exit();
+          }, 8000)
+        } else if (message.action == "restart") {
+          this.log(`[Cloud Core] ${usertext}Restarting the server...`);
+          this.stop(() => {
+            this.log(`[Cloud Core] Starting the server...`);
+            this.start();
+          });
+        } else if (message.action == "start") {
+          this.log(`[Cloud Core] ${usertext}Starting the server...`);
+          this.start();
+        }
+      });
+
+      //send console to client
+      this.on("console", (line) => {
+        socket.send(line);
+      })
+    });
+
+    let app = express();
+
+    app.use(express.json());
+
+    //define api routes
+    app.get("*", (req, res, next) => {
+      res.set('Access-Control-Allow-Origin', '*');
+      if (req.headers['authorization'] == config.core.authorization) {
+        next();
+      } else {
+        res.type("json");
+        res.send(JSON.stringify(
+          {
+            "error": {
+              "code": 502, 
+              "reason": "Unauthorized. This usually means you have the wrong authentication code in your header."
+            }
+          }
+        ));
+      }
+    })
+
+    app.post("*", (req, res) => {
+      //post to minecraft
+      if (req.body.command) {
+        if (req.body.command == "restart") {
+          this.log(`[Cloud Core] Restarting the server...`);
+          this.stop(() => {
+            this.log(`[Cloud Core] Starting the server...`);
+            this.start();
+          });
+        } else if (req.body.command == "start") {
+          this.start();
+        } else {
+          this.send(req.body.command, req.body.user || undefined);
+        }
+        res.end();
+      } else {
+        res.end();
+      }
+    })
+
+    app.get("*/usage", (req, res) => {
+      res.type("json");
+      if (this.minecraftServer) {
+        usage.lookup(this.minecraftServer.pid, function(err, result) {
+          if (!err) {
+            res.end(JSON.stringify(result));
+          } else {
+            res.end(JSON.stringify({"error": err}));
+          }
+        });
+      } else {
+        res.end(JSON.stringify({"error": "Server not started"}));
+      }
+    })
+
+    app.get("*", (req, res) => {
+      const readLastLines = require('read-last-lines');
+      readLastLines.read('logs/latest.log', 100)
+	      .then((lines) => {
+          res.end(lines)
+        });
+    })
+
+    //listen on server
+    this.httpServer = app.listen(config.remote.port, config.remote.bind || "0.0.0.0", () => {
+      this.log(`[Cloud Core] Started webserver running on ${config.remote.bind || "0.0.0.0"}:${config.remote.port}`)
+    });
+
+    //upgrade websocket requests
+    this.httpServer.on('upgrade', (request, socket, head) => {
+      this.wsServer.handleUpgrade(request, socket, head, socket => {
+        //make sure auth is correct, specified in the protocol location
+        if (request.headers['sec-websocket-protocol'] == config.core.authorization) {
+          this.wsServer.emit('connection', socket, request);
+        } else {
+          socket.send("[Cloud Core] Unauthorized. This usually means you have the wrong authentication code in your header.");
+          socket.close(1000, "Unauthorized");
+        }
+      });
+    });
+
+    //auto backups
+    if (config.core.backups.enabled) {
+      this.log(`[Cloud Core] Automatic Server Backups enabled. Backups will be taken ${config.core.backups.time}.`)
+      
       setTimeout(() => {
         let d = new Date();
-        if (d.getDay() == 0 && d.getHours() == 0 && d.getSeconds() <= 59 && d.getMinutes() == 0) {
-          this.backup()
+        if (config.core.backups.time == "weekly") {
+          if (d.getMinutes() == 1 && d.getHours() == 1 && d.getDay() == 1) {
+            this.backup();
+          }
+        } else if (config.core.backups.time == "monthly") {
+          if (d.getMinutes() == 1 && d.getHours() == 1 && d.getDate() == 1) {
+            this.backup();
+          }
         }
       }, 60000)
     }
-    this.checkUpdates()
+
+    //emit that we are ready to go
+    setTimeout(() => {
+      this.emit("ready", config);
+    }, 500)
+
+    return this;
   }
-  
-  /**
-   * Checks git repo for updates
-   */
-  checkUpdates() {
-    axios.get('https://api.github.com/repos/Chezzer-ub/cloud-core/commits?path=cloud-core.js&per_page=1')
-      .then(function (response) {
-        let data = response.data;
-        if (!fs.existsSync("cloud-core.json")) {
-            fs.writeFileSync("cloud-core.json", `
-              {
-                "version": "${data[0].sha}"
-              }
-            `)
-        } else {
-          let version = "";
-          try {
-            version = JSON.parse(fs.readFileSync("cloud-core.json"))['version'];
-          } catch (e) {
-            console.log(e);
-          }
-          if (data[0].sha !== version) {
-            setTimeout(() => {
-              messageHandler.emit('message', `OUTDATED VERSION! A new version of Cloud Core is out! Please follow the update instructions on the README. https://github.com/Chezzer-ub/cloud-core. /tWARN`)
-              console.log("A new version of Cloud Core is out! Please follow the update instructions on the README. https://github.com/Chezzer-ub/cloud-core")
-            }, 10000)
-          }
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+
+  log(msg) {
+    fs.appendFileSync("logs/latest.log", `${msg}\n`);
+    this.emit('console', msg);
+    console.log(msg);
   }
 
   /**
-  * Backs up the server into the ./backups directory
-  * 
-  * @return {error} Error when backing up.
-  */
+   * Send command to server
+   *
+   * @param {String} command Command to send to server
+   * @param {String} user Optional. User to assign command to
+   */
+  send(command, user) {
+    //command event
+    this.emit("command", command);
+
+    //send to minecraft server
+    this.minecraftServer.stdin.write(`${command}\n`);
+
+    //send to websocket
+    this.log(`> ${command} ${user ? "(Issued by "+user+")" : ""}`);
+  }
+
+  /**
+   * Starts Minecraft server
+   *
+   * @return {ChildProcessWithoutNullStreams} Started Minecraft server child process.
+   */
+  start() {
+    if (!this.minecraftServer) {
+      //make args
+      let args = this.config.core.args.concat('-jar', this.config.core.jar);
+      args = args.concat('--port', this.config.core.port, 'nogui');
+      
+      //start server
+      this.minecraftServer = spawn('java', args);
+      
+      //minecraft to console
+      this.minecraftServer.stdout.pipe(process.stdout);
+      
+      //console to minecraft
+      process.stdin.pipe(this.minecraftServer.stdin);
+      
+      //minecraft to event
+      this.minecraftServer.stdout.on('data', (d) => {
+        d.toString().split('\n').forEach((l) => {
+          if (l) this.emit('console', l.trim());
+        });
+      });
+
+      this.minecraftServer.on('exit', () => {
+        this.minecraftServer.kill();
+        this.minecraftServer = null;
+      })
+      //start event
+      this.emit("start");
+    }
+    return this.minecraftServer;
+  }
+
+  /**
+   * Stops server
+   *
+   * @param {Function} callback Callback when server is stopped
+   */
+  stop(callback) {
+    //stopping event
+    this.emit("stopping");
+    //stop server
+    if (this.minecraftServer) {
+      //write stop command
+      this.minecraftServer.stdin.write("stop\n");
+      this.minecraftServer.on('exit', () => {
+        //on exit
+        this.minecraftServer.kill();
+        this.minecraftServer = null;
+        if (callback) {
+          callback();
+        }
+        //stop event
+        this.emit("stop");
+      });
+    } else {
+      if (callback) {
+        callback();
+      }
+      //stop event
+      this.emit("stop");
+    }
+  }
+
+  /**
+   * Backup the whole server.
+   */
   backup() {
+    if (!fs.existsSync(this.config.core.backups.directory)) {
+      fs.mkdirSync(this.config.core.backups.directory);
+    }
     let d = new Date();
-    let name = d.toString().replace(/ /g, "-").split("-(")[0]+".zip"
-    let backup = exec(`zip -r ${name} * -x *backup*`, {maxBuffer: 1024 * 999999999}, (error) => {
-    //let backup = exec(`cd..;sleep 10;ls`, (error, stdout, stderr) => {
+    let name = d.toString().replace(/ /g, "-").split("-(")[0]+".zip";
+    this.log(`[Cloud Core] Starting server backup...`);
+    let backup = exec(`zip -r ${this.config.core.backups.directory}${name} * -x *backup*`, {maxBuffer: 1024 * 999999999}, (error) => {
       if (error) {
+        this.log(`[Cloud Core] Backup Error: ${error.code}`);
         console.log(error.stack);
-        console.log('Error code: '+error.code);
-        console.log('Signal received: '+error.signal);
-        return error;
       }
     });
     
     backup.on('exit', () => {
-      setTimeout(() => {
-        exec(`mv ${name} backup/${name}`, {maxBuffer: 1024 * 999999999})
-      }, 2000)
-      fs.appendFileSync("logs/latest.log", `Made backup (${name})`)
-      messageHandler.emit('message', `Made backup of whole server (${name}). /tINFO`)
-    });
-  }
-  /**
-   * Starts Minecraft server
-   *
-   * @return {server} Started Minecraft server.
-   */
-  start() {
-    if (this.spawn) {
-      messageHandler.emit('message', `Server already started. /tERROR`)
-      return false;
-    };
-    messageHandler.emit('message', `Starting Server. /tINFO`)
-    console.log("Starting server, please wait...")
-
-    let args = this.config.core.args.concat('-jar', this.config.core.jar);
-    args = args.concat('--port', this.config.core.port, 'nogui')
-    const config = this.config;
-
-    this.spawn = spawn('java', args);
-
-    this.spawn.stdout.pipe(process.stdout);
-    process.stdin.pipe(this.spawn.stdin);
-
-    this.spawn.stdout.on('data', (d) => {
-      // Emit console
-      d.toString().split('\n').forEach((l) => {
-        if (l) this.emit('console', l.trim());
-      });
-    });
-
-    if (!this.wsServer) { 
-      var server = this;
-      //define ws server
-      var httpServer = http.createServer(function(req, res) {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-        res.setHeader("Access-Control-Allow-Headers","*")
-        if (req.headers.authorization == "Basic "+config.core.authorization) {
-          if (req.method == 'POST') {
-            let body = '';
-
-            req.on('data', chunk => {
-              body += chunk.toString();
-            });
-  
-            req.on('end', () => {
-              if (body) {
-                let command = decodeURIComponent(body);
-                command = command.replace(/\+/g," ");
-                command = command.split("command=")[1];
-                setTimeout(() => {
-                  server.send(command)
-                }, 200)
-              }
-            });
-            res.end();
-          } else {
-            if (req.query.request == "usage") {
-              var usage = require('usage');
-              let pid = this.spawn.pid
-              
-              usage.lookup(pid, function(err, result) {
-                res.end(JSON.stringify(result));
-              });
-            } else {
-              if (fs.existsSync("logs/latest.log")) {
-                const log = fs.readFileSync("logs/latest.log", "utf8");
-                res.write(log);
-                res.end();
-              } else {
-                res.end();
-              }
-            }
-          }
-        } else {
-          res.end("Unauthorized");
-        }
-        res.end();
-      }).listen(this.config.remote.port, () => {
-        console.log("Server remote running on port " + this.config.remote.port);
-      });
-
-      //make it
-      this.wsServer = new WebSocketServer({
-        httpServer: httpServer,
-        autoAcceptConnections: false
-      });
-
-      var server = this;
-
-      this.wsServer.on('request', function(req) {
-        
-        var connection = req.accept();
-        server.on('console', line => {
-          connection.sendUTF(line);
-        })
-
-        messageHandler.on('message', msg => {
-          let data = msg.split("/t");
-          let type;
-          if (!data[1]) {
-            type = "INFO"
-          } else {
-            type = data[1];
-          }
-
-          msg = data[0];
-
-          let d = new Date();
-          let hours = d.getHours();
-          let mins  = d.getMinutes();
-          let secs  = d.getSeconds();
-          if (hours < 10) {
-            hours = "0"+hours;
-          }
-          if (mins < 10) {
-            mins = "0"+mins;
-          }
-          if (secs < 10) {
-            secs = "0"+secs;
-          }
-          connection.sendUTF(`[${hours}:${mins}:${secs} ${type}]: [Cloud Core] ${msg}`);
-        })
-
-        connection.on('message', function(message) {
-          let body = JSON.parse(message.utf8Data);
-          if (body.authorization == config.core.authorization) {
-            server.send(body.command)
-          } else {
-            connection.sendUTF("Unauthorized");
-          }
-        })
-      })
-    }
-    this.checkUpdates()
-
-    return this;
-  }
-  /**
-   * Stops server
-   *
-   * @return {server} Stopped Minecraft server.
-   */
-  stop() {
-    messageHandler.emit('message', `Stopping Server. /tINFO`)
-    if (this.spawn) {
-
-      this.spawn.kill();
-      this.spawn = null;
-    }
-
-    return this;
-  }
-
-  use(module) {
-    if (typeof module !== 'function') throw new Error('A module must be a function');
-
-    if (this.modules.filter(m => m === module).length === 0) {
-      this.modules.push(module);
-      module.call(this);
-    }
-
-    return this;
-  }
-  /**
-   * Sends command to server
-   *
-   * @param {string} command The command to send.
-   * @return {Promise} Promise when command is finished.
-   */
-  send(command) {
-    return new Promise((resolve) => {
-      if (command == "start" || command == "/start") {
-        messageHandler.emit('message', `Received start command. /tINFO`)
-        this.start();
-        resolve()
-      } else if (command == "restart" || command == "/restart") {
-        messageHandler.emit('message', `Received stop command. /tINFO`)
-        this.stop();
-        setTimeout(() => {
-          this.start();
-          resolve()
-        }, this.config.core.restartTime ? this.config.core.restartTime*1000 : 10000)
-      } else if (command == "backup" || command == "/backup") {
-        messageHandler.emit('message', `Received backup command. /tINFO`)
-        this.backup();
-        resolve();
-      } else if (command == "dkill") {
-        messageHandler.emit('message', `Received daemon kill command. /tINFO`)
-        this.stop();
-        setTimeout(function () {
-          resolve()
-          process.exit();
-        }, this.config.core.restartTime*1000 || 10000);
-      } else {
-        messageHandler.emit('message', `Received "${command}". /tINFO`)
-        this.spawn.stdin.write(`${command}\n`, () => resolve());
-      }
+      this.emit("backup", `${this.config.core.backups.directory}${name}`);
+      this.log("logs/latest.log", `[Cloud Core] Made backup (${this.config.core.backups.directory}${name})`)
     });
   }
 }
 
 module.exports = CloudCore;
-
